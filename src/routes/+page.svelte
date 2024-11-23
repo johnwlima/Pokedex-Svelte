9853f2cb21fb846199c89c19650d3fcbcebbb534
John Lima <jwesley1210@gmail.com>
2024-11-23 17:46:34 -0300

    feat: add card style

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,13 +6,23 @@
     export let data: PageData;
 </script>
 
-
-{#each data.monsters as monster, i (monster.url) }
-   <p class="text-xl font-bold">{i+1}: {monster.name}</p>
-{/each}
-
 {#each generations as generation (generation.id)}
     <h1>{generation.name}</h1>
     <p>Games: {generation.games.join(", ")}</p>
     <p>Main region: {generation.main_region}</p>
 {/each}
+
+<div id="pokemon-container" class="items-start flex-row justify-start w-creen flex flex-wrap basis-24">
+    {#each data.monsters as monster (monster.id) }
+    <div id="pokemon-card" class="max-w-48 grow drop-shadow-sm relative m-2 w-34 h-42 rounded bg-slate-100 border-slate-200 border-2 p-4 flex flex-col flex-center text-center hover:bg-slate-50 hover:border-slate-100 cursor-pointer"> 
+        <span class="font-medium  text-slate-500 absolute top-1 right-3 text-xs" id="pokemon-id">{monster.id}</span>
+        <p class="font-bold first-letter:uppercase text-slate-700"> {monster.name}</p>
+        <img src={monster.image} alt={monster.name}>
+    </div>
+    
+    {/each}
+</div>
+
+<style>
+
+</style>
--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,25 +1,22 @@
 import type { PageLoad } from "./$types"
-
 type IndexMonster = {
-    name: string
-    url: string
+  name: string
+  url: string
 }
-
-export const load = async({fetch}) => {
-    const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
-    const json = await response.json()
-    const monsters = json.results.map((monster: IndexMonster) => {
-        const splitUrl = monster.url.split("/")
-        const id = splitUrl[splitUrl.length - 2]
-
-        return {
-            name: monster.name,
-            url: monster.url,
-            id
-        }
-    })
-
+export const load = (async ({ fetch }) => {
+  const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
+  const json = await response.json()
+  const monsters = json.results.map((monster: IndexMonster) => {
+    const splitUrl = monster.url.split('/')
+    const id = splitUrl[splitUrl.length - 2]
     return {
-        monsters: json.results
+      name: monster.name,
+      url: monster.url,
+      id,
+      image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`
     }
-}
+  })
+  return {
+    monsters
+  }
+}) satisfies PageLoad
