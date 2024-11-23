8040507e87c8eb91ae77146644d527bbeb283a22
John Lima <jwesley1210@gmail.com>
2024-11-23 15:04:54 -0300

    feat: Create structure of Pokemon Input

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+    import {generations} from "./generations";
+</script>
+
+{#each generations as generation}
+    <h1>{generation.name}</h1>
+    <p>Games: {generation.games.join(', ')}</p>
+    <p>Main region: {generation.main_region}</p>
+{/each}
