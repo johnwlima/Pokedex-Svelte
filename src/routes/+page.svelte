fb7a48cbe6bdf9a127ac5e24d909965ff9f3fdb9
John Lima <jwesley1210@gmail.com>
2024-11-30 13:00:54 -0300

    style: format documents to Standardize code writing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,83 +1,78 @@
 <script lang="ts">
+	import type { PageData } from './$types';
+	import { page } from '$app/stores';
+	import { generations } from './generations';
+	import { goto } from '$app/navigation';
+	import Monster from '$lib/components/Monster.svelte';
 
-    import type { PageData } from "./$types";
-    import { page } from "$app/stores";
-    import { generations } from "./generations";
-    import { goto } from "$app/navigation";
-	import Monster from "$lib/components/Monster.svelte";
+	export let data: PageData;
 
-    export let data: PageData;
+	let form = {
+		searchString: ''
+	};
 
-    let form =  {
-        searchString: ''
-    }
+	let searchString = '';
+	$: selectedMonsters = data.monsters.filter((monster) => {
+		return monster.name.toLowerCase().includes(searchString.toLowerCase());
+	});
 
-    let searchString = ""
-    $: selectedMonsters = data.monsters.filter((monster) => {
-        return monster.name.toLowerCase().includes(searchString.toLowerCase())
-    })
+	$: monsterId = $page.url.searchParams.get('monsterId') || '';
+	$: monster = data.monsters.find((monster) => monster.id === monsterId);
+	$: monsterId2 = $page.url.searchParams.get('monsterId2') || '';
+	$: monster2 = data.monsters.find((monster) => monster.id === monsterId2);
 
-    $: monsterId = $page.url.searchParams.get("monsterId") || "";
-    $: monster = data.monsters.find((monster) => monster.id === monsterId);
-    $: monsterId2 = $page.url.searchParams.get("monsterId2") || "";
-    $: monster2 = data.monsters.find((monster) => monster.id === monsterId2);
+	const updateSearchParams = (key: string, value: string) => {
+		const searchParams = new URLSearchParams($page.url.searchParams);
+		searchParams.set(key, value);
+		goto(`?${searchParams.toString()}`);
+	};
 
-    const updateSearchParams = (key: string, value: string) => {
-        const searchParams = new URLSearchParams($page.url.searchParams);
-        searchParams.set(key, value);
-        goto(`?${searchParams.toString()}`)
-    }
-
-    const submitSearch = (e:Event) => {
-        searchString = form.searchString
-    }
-
+	const submitSearch = (e: Event) => {
+		searchString = form.searchString;
+	};
 </script>
 
 {#if monster}
-  <Monster
-    monster={monster}
-    updateSearchParams={updateSearchParams}
-    isInteractive={false}
-  />
+	<Monster {monster} {updateSearchParams} isInteractive={false} />
 {/if}
 {#if monster2}
-  <Monster
-    monster={monster2}
-    updateSearchParams={updateSearchParams}
-  />
+	<Monster monster={monster2} {updateSearchParams} />
 {/if}
 
-<div id="generations-container" class="flex flex-row flex-wrap w-full justify-center">
-    {#each generations as generation (generation.id)}
-        <button id="generation-button" class="m-4 py-1 px-3 border border-slate-100 rounded-lg bg-slate-600 text-slate-50 font-bold hover:bg-slate-700">
-            {generation.main_region}
-        </button> 
-    {/each}
+<div id="generations-container" class="flex w-full flex-row flex-wrap justify-center">
+	{#each generations as generation (generation.id)}
+		<button
+			id="generation-button"
+			class="m-4 rounded-lg border border-slate-100 bg-slate-600 px-3 py-1 font-bold text-slate-50 hover:bg-slate-700"
+		>
+			{generation.main_region}
+		</button>
+	{/each}
 </div>
 
-<form onsubmit= {submitSearch}
-    class="w-full flex justify-center items-center m-4 gap-2">
+<form onsubmit={submitSearch} class="m-4 flex w-full items-center justify-center gap-2">
+	<input
+		type="text"
+		placeholder="Busque um Pokemon"
+		bind:value={form.searchString}
+		class="rounded bg-slate-200 px-4 py-2 text-xl text-slate-800 placeholder:text-slate-400"
+	/>
 
-    <input type="text" placeholder="Busque um Pokemon" bind:value={form.searchString}
-        class="bg-slate-200 py-2 px-4 rounded text-xl text-slate-800 placeholder:text-slate-400"
-    />
-
-    <input type="submit" value="Pesquisar"
-        class="py-2.5 px-3 border border-slate-100 rounded-lg bg-slate-600 text-slate-50 font-bold hover:bg-slate-700"
-    >
+	<input
+		type="submit"
+		value="Pesquisar"
+		class="rounded-lg border border-slate-100 bg-slate-600 px-3 py-2.5 font-bold text-slate-50 hover:bg-slate-700"
+	/>
 </form>
 
-<div id="pokemon-container" class="items-start flex-row justify-start w-creen flex flex-wrap basis-24">
-    {#each selectedMonsters as monster (monster.id) }
-        <Monster
-            monster={monster}
-            updateSearchParams={updateSearchParams}
-            isInteractive={true}
-        /> 
-    {/each}
+<div
+	id="pokemon-container"
+	class="w-creen flex basis-24 flex-row flex-wrap items-start justify-start"
+>
+	{#each selectedMonsters as monster (monster.id)}
+		<Monster {monster} {updateSearchParams} isInteractive={true} />
+	{/each}
 </div>
 
 <style>
-
 </style>
--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,29 +1,29 @@
-import type { PageLoad } from "./$types" //estudar isso
+import type { PageLoad } from './$types';
 type ApiMonster = {
-  name: string
-  url: string
-}
+	name: string;
+	url: string;
+};
 
 export type IndexMonster = ApiMonster & {
-    id: string
-    url: string
-    image: string
-}
+	id: string;
+	url: string;
+	image: string;
+};
 
 export const load = (async ({ fetch }) => {
-  const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
-  const json = await response.json()
-  const monsters: IndexMonster[] = json.results.map((monster: ApiMonster) => {
-    const splitUrl = monster.url.split('/')
-    const id = splitUrl[splitUrl.length - 2]
-    return {
-      name: monster.name,
-      url: monster.url,
-      id,
-      image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`
-    }
-  })
-  return {
-    monsters
-  }
-}) satisfies PageLoad //estudar isso
+	const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151');
+	const json = await response.json();
+	const monsters: IndexMonster[] = json.results.map((monster: ApiMonster) => {
+		const splitUrl = monster.url.split('/');
+		const id = splitUrl[splitUrl.length - 2];
+		return {
+			name: monster.name,
+			url: monster.url,
+			id,
+			image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`
+		};
+	});
+	return {
+		monsters
+	};
+}) satisfies PageLoad;
