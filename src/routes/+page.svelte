f35416436f0ee5aabc79771040e9d5f73eb581f0
John Lima <jwesley1210@gmail.com>
2024-11-30 12:55:04 -0300

    feat: Create Search Bar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,6 +8,15 @@
 
     export let data: PageData;
 
+    let form =  {
+        searchString: ''
+    }
+
+    let searchString = ""
+    $: selectedMonsters = data.monsters.filter((monster) => {
+        return monster.name.toLowerCase().includes(searchString.toLowerCase())
+    })
+
     $: monsterId = $page.url.searchParams.get("monsterId") || "";
     $: monster = data.monsters.find((monster) => monster.id === monsterId);
     $: monsterId2 = $page.url.searchParams.get("monsterId2") || "";
@@ -17,6 +26,10 @@
         const searchParams = new URLSearchParams($page.url.searchParams);
         searchParams.set(key, value);
         goto(`?${searchParams.toString()}`)
+    }
+
+    const submitSearch = (e:Event) => {
+        searchString = form.searchString
     }
 
 </script>
@@ -43,8 +56,20 @@
     {/each}
 </div>
 
+<form onsubmit= {submitSearch}
+    class="w-full flex justify-center items-center m-4 gap-2">
+
+    <input type="text" placeholder="Busque um Pokemon" bind:value={form.searchString}
+        class="bg-slate-200 py-2 px-4 rounded text-xl text-slate-800 placeholder:text-slate-400"
+    />
+
+    <input type="submit" value="Pesquisar"
+        class="py-2.5 px-3 border border-slate-100 rounded-lg bg-slate-600 text-slate-50 font-bold hover:bg-slate-700"
+    >
+</form>
+
 <div id="pokemon-container" class="items-start flex-row justify-start w-creen flex flex-wrap basis-24">
-    {#each data.monsters as monster (monster.id) }
+    {#each selectedMonsters as monster (monster.id) }
         <Monster
             monster={monster}
             updateSearchParams={updateSearchParams}
