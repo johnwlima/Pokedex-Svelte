8286b98a17b6115ae98de9aa608863fbbd2da6a2
John Lima <jwesley1210@gmail.com>
2024-12-01 20:55:13 -0300

    feat: Add timer

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,18 +1,25 @@
 <script lang="ts">
+	import Timer from "./Timer.svelte";
+
 </script>
 
-<div 
-class="p-6 mb-4 w-full bg-slate-900 flex items-center justify-around gap-4">
-
-    <a href="/" id="logo" class="flex gap-2 items-center">
-        <img src="/pokemon-icon.jpg" alt="pokemon icon" class="w-12">
-        <span class="text-lg font-bold text-slate-50">Pokedex</span>
+{#snippet link(content: string = '', link: string = '/')}
+    <a href="{link}" class="bg-slaterounded-lg px-3 py-2.5 font-bold text-slate-50 rounded-lg hover:border-b-2 border-blue-300 hover:text-blue-300"> {content}
     </a>
+{/snippet}
 
-    {#snippet link(content: string = '', link: string = '/')}
-        <a href="{link}" class="bg-slaterounded-lg px-3 py-2.5 font-bold text-slate-50 rounded-lg hover:border-b-2 border-blue-300 hover:text-blue-300"> {content}
+<div 
+class="p-6 mb-4 w-full bg-slate-900 flex items-center content-center justify-around gap-4">
+
+    <div class="flex items-center content-center gap-2 ">
+        <a href="/" id="logo" class="flex gap-2 items-center">
+            <img src="/pokemon-icon.jpg" alt="pokemon icon" class="w-12">
+            <span class="text-lg font-bold text-slate-50">Pokedex</span>
         </a>
-    {/snippet}
+        <Timer/>
+    </div>
+
+    
 
     <nav class="flex items-center justify-start gap-4">
        {@render link("Home","/")}
---
+++ src/lib/components/Timer.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	let time: string;
+  
+	function updateTime() {
+	  const now = new Date();
+	  time = now.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', second: '2-digit' }).replace(/:/g, ':');
+	}
+  
+	onMount(() => {
+	  updateTime();
+	  const interval = setInterval(updateTime, 1000);
+	  return () => clearInterval(interval);
+	});
+  </script>
+  
+  <div 
+	  class="text-slate-50 font-medium bg-slate-800 p-2 h-10 rounded-lg text-md tracking-widest flex items-center justify-center"> <span>Time</span>
+	  {time}
+  </div>
+  
+  <style>
+  </style>
+  
